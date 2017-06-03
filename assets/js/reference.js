de1d56953bc1d3b1bf1e00b4ab56512bb7a62cf6
Jasper.J <jasper.jardin1994@gmail.com>
2017-06-03 15:42:53 +0800

    Fixed the Reference table of contents.

--- assets/js/reference.js
+++ assets/js/reference.js
@@ -128,7 +128,7 @@ jQuery(document).ready( function($) {
         });
     }
 
-    $( '.reference-menu-container ol li ol.sub-menu li.menu-item-has-children > a' ).after( '<span id="thrive_nav_btn" class="pages-menu-toggle"></span>' );
+    $( '.reference-menu-container li.menu-item-has-children > a' ).after( '<span id="thrive_nav_btn" class="pages-menu-toggle"></span>' );
 
     $('.pages-menu-toggle').on('click', function(e) {
 
